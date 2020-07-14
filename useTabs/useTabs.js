c92eb71e95fbfbff1d83353ad74f7abba7658d33
auroville <auroville.pms@gmail.com>
2020-07-14 17:29:16 +0900

    Add : useTabs 구현

---
+++ useTabs/useTabs.js
@@ -0,0 +1,25 @@
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+  {
+    tab: "Section 3",
+    content: "I'm the content of the Section 3",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
